845df72cdd2e82348cf6dedf6b99bce003607ea6
LeoT <LeoT>
2020-04-25 21:10:12 +0200

    JavaFX Setup Test

---
+++ LerneJava/src/module-info.java
@@ -0,0 +1,8 @@
+module TestFX {
+	requires javafx.fxml;
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.web;
+	
+	opens unterordner;
+}
---
+++ LerneJava/src/unterordner/Tester.java
@@ -0,0 +1,18 @@
+package unterordner;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Tester extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+		
+	}
+	
+}
