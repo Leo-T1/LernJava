6469584249a37dcf73a07dbd7a1fcbbca1c10f7f
LeoT <LeoT>
2020-04-25 20:11:09 +0200

    Zweite Aufgabe Hochladen

---
+++ LernJava/src/unterordner/Aufgabe1.java
@@ -0,0 +1,11 @@
+package unterordner;
+
+public class Aufgabe1 {
+	public static void main(String[] args) {
+		int auto1 = 3500;
+		double auto2 = 2340.48;
+		int auto2int = (int)auto2;
+		int gesamt = auto1 +auto2int;
+		System.out.println("Gesamt:"+ gesamt);
+	}
+}
