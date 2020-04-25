4e478bd9010399bfab0f793ab97faf7029b94ac8
LeoT <LeoT@DESKTOP-122504J.fritz.box>
2020-04-25 21:31:27 +0200

    Main file

--- LerneJava/src/unterordner/Main.java
+++ LerneJava/src/unterordner/Main.java
@@ -4,23 +4,11 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	public static void main(String[] args) {
-		System.out.println("Vor Launch");
-		launch(args);
-		System.out.println("Nach Launch");
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 	
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		System.out.println("Start Methode");
-	}
-	
-	@Override
-	public void init() throws Exception {
-		System.out.println("Init Methode");
-	}
-	@Override
-	public void stop() throws Exception {
-		System.out.println("Stop Methode");
-	}
 }
