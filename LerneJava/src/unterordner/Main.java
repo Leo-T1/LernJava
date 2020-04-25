95f12621165cafc54b3876876270f5aaaeabb2f4
LeoT <LeoT@DESKTOP-122504J.fritz.box>
2020-04-25 21:44:27 +0200

    3 Video mitschreiben

--- LerneJava/src/unterordner/Main.java
+++ LerneJava/src/unterordner/Main.java
@@ -1,7 +1,11 @@
 package unterordner;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
 
 public class Main extends Application {
 	public static void main(String[] args) {
@@ -12,7 +16,27 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		System.out.println("Start Methode");
+		try {
+			System.out.println("Start Methode");
+			VBox root = new VBox();
+			VBox root2 = new VBox();
+			
+			Label text = new Label("Hello World");
+			root.getChildren().add(text);
+			Scene scene = new Scene(root,400,400);
+			Scene scene2 = new Scene(root2,200,200);
+			
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			Stage secoundStage = new Stage();
+			secoundStage.setScene(scene2);
+			secoundStage.show();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
