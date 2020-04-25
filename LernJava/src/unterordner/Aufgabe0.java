40a822574dd2c74a957ff79a5ad2a0898a19e069
LeoT <LeoT@DESKTOP-122504J.fritz.box>
2020-04-25 19:30:03 +0200

    Erste Aufgabe Hochladen und Dummy-Dateien Entfernen

---
+++ LernJava/src/unterordner/Aufgabe0.java
@@ -0,0 +1,10 @@
+package unterordner;
+
+public class Aufgabe0 {
+	public static void main(String[] args) {
+		int auto = 3500;
+		int alter = 18;
+		String name = "Peter Müller";
+		System.out.println("Name: "+name+", Alter: "+alter+", sein Auto kostet: "+auto+"€");
+	}
+}
