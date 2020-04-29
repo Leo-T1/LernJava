367fc28b4ace495c7ba807ceb42a133fca949bd3
LeoT <LeoT@DESKTOP-122504J.fritz.box>
2020-04-29 22:22:51 +0200

    4.2 Video mitschreiben

--- LerneJava/src/module-info.java
+++ LerneJava/src/module-info.java
@@ -4,6 +4,7 @@ module TestFX {
 	requires javafx.graphics;
 	requires javafx.web;
 	requires javafx.base;
+	requires java.desktop;
 	
 	opens unterordner;
 	//https://www.youtube.com/watch?v=B-mgPyXEIgo
--- LerneJava/src/unterordner/Main.java
+++ LerneJava/src/unterordner/Main.java
@@ -1,29 +1,58 @@
 package unterordner;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
 
 public class Main extends Application {
+	int rotate = 0;
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		try {
 			VBox root = new VBox();
 			
-			Label label = new Label("Hallo dies ist ein viel viel viel viel viel viel viel zu langes label");
+			Label label = new Label("Hallo dies ist ein viel viel viel viel viel viel viel viel viel viel zu langes label");
 			root.getChildren().add(label);
 			//label.setRotate(180);
-			label.setUnderline(true);
-			label.setTextFill(Color.BROWN);
+			//label.setUnderline(true);
+			//label.setTextFill(Color.BROWN);
 			label.setWrapText(true);
+			label.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent arg0) {
+					label.setUnderline(true);
+				}
+				
+			});
+			label.setOnMouseExited(new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent arg0) {
+					label.setUnderline(false);
+				}
+				
+			});
+			
+			label.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent arg0) {
+					rotate+=10;
+					label.setRotate(rotate);
+				}
+				
+			});
+			
 			
 			Scene scene = new Scene(root,300,300);
 			
