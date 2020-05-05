25ebe4b3578d33e6ffaf95fff44ff51a804fbc2b
LeoT <LeoT@DESKTOP-122504J.fritz.box>
2020-05-05 17:08:47 +0200

    4.10 Video mitschreiben

--- LerneJava/src/unterordner/Main.java
+++ LerneJava/src/unterordner/Main.java
@@ -20,42 +20,22 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		try {
 			VBox root = new VBox();
-			Scene scene = new Scene(root,400,300);
-			
-			Label labelName = new Label("Username:");
-			TextField nameField = new TextField();
-			Label labelPwd = new Label("Passwort:");
-			PasswordField pwdField = new PasswordField();
-			Label labelErr = new Label("");
-			labelErr.setTextFill(Color.RED);
-			Button sendButton = new Button("Login");
+			Scene scene = new Scene(root,300,300);
+			TextArea areaField = new TextArea();
+			areaField.setPrefHeight(200);
+			areaField.setText("Handrik ist der Dozenz von diesem Kurs, denn er programmiert abscheinend sehr gerne. Er freut sich anscheinend das ich bei ihm zugucke.");
+			areaField.setWrapText(true);
+			Button sendButton = new Button("Abschicken");
 			sendButton.setOnAction(new EventHandler<ActionEvent>() {
 
 				@Override
 				public void handle(ActionEvent arg0) {
-					String name = nameField.getText();
-					String pwd = pwdField.getText();
-					
-					if((name.isBlank()) && (pwd.isBlank())){
-						labelErr.setText("Bitte Benutzername und Passwort eingeben.");
-					}else if((name.isBlank())) {
-						labelErr.setText("Bitte Benutzername eingeben.");
-					}else if((pwd.isBlank())) {
-						labelErr.setText("Bitte Passwort eingeben.");
-					}else if(name.equals("gamer10") && pwd.equals("passwort123")){
-						labelErr.setText("Erfolgreich eingeloggt! Du wirst gleich weitergeleitet...");
-						labelErr.setTextFill(Color.GREEN);
-						nameField.setDisable(true);
-						pwdField.setDisable(true);
-						
-					}else {
-						labelErr.setText("Ungültiger Username oder ungültiges Passwort!");
-					}
+					System.out.println(areaField.getText());
 				}
 				
 			});
 			
-			root.getChildren().addAll(labelName,nameField,labelPwd,pwdField,sendButton,labelErr);
+			root.getChildren().addAll(areaField,sendButton);
 			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
