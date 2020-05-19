13e93da37573903b54b9155b785bcc27e549c6bf
LeoT <LeoT@DESKTOP-122504J>
2020-05-19 18:19:39 +0200

    4.39 Video mitschreiben

--- LerneJava/src/unterordner/Main.java
+++ LerneJava/src/unterordner/Main.java
@@ -3,6 +3,12 @@ package unterordner;
 
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import javax.swing.event.ChangeEvent;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -37,22 +43,24 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		try {
 			VBox root = new VBox();
-			Scene scene = new Scene(root,900,700);
+			Scene scene = new Scene(root,400,300);
 			
-			HTMLEditor edit = new HTMLEditor();
+			TextArea ta = new TextArea();
+			Button open = new Button("Datei öffnen");
 			
-			Button but = new Button("Send");
-			but.setOnAction(new EventHandler<ActionEvent>() {
+			open.setOnAction(new EventHandler<ActionEvent>() {
 
 				@Override
 				public void handle(ActionEvent arg0) {
-					System.out.println(edit.getHtmlText());
+					FileChooser fc = new FileChooser();
+					File f = fc.showOpenDialog(null);
+					ta.setText(readLine(f));
 				}
 				
 			});
 			
 			
-			root.getChildren().addAll(edit,but);
+			root.getChildren().addAll(ta,open);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("-");
@@ -64,6 +72,25 @@ public class Main extends Application {
 		}
 	}
 	
+	public static String readLine(File f) {
+		
+		try {
+			FileReader fr = new FileReader(f.getAbsolutePath());
+			BufferedReader br = new BufferedReader(fr);
+			String firstLine = null;
+			try {
+				firstLine = br.readLine();
+			} catch (IOException e) {
+				
+			}
+			return firstLine;
+		} catch (FileNotFoundException e) {
+			
+		}
+		
+		return null;
+	}
+
 	public ObservableList<Car> getAllCars(){
 		ObservableList<Car> carList = FXCollections.observableArrayList();
 		carList.add(new Car("Ibiza", "Seat", 110));
