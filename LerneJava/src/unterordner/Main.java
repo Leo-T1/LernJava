df4b0b3b86d0a384b243d68cd9341c7edcc85405
LeoT <LeoT@DESKTOP-122504J>
2020-05-27 16:33:35 +0200

    6.8 Aufgabe bearbeiten

--- LerneJava/src/unterordner/Main.java
+++ LerneJava/src/unterordner/Main.java
@@ -25,6 +25,7 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeLineCap;
 import javafx.scene.text.Font;
 import javafx.scene.web.HTMLEditor;
@@ -40,35 +41,53 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		try {
-			Group root = new Group();
-			Scene scene = new Scene(root,350,250);
+			VBox set = new VBox(10);
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,450,350);
+			Label l1 = new Label("Höhe:");
+			Label l2 = new Label("Breite:");
+			Label l3 = new Label("Farbe:");
+			
+			Slider setHig = new Slider(10, 250, 10);
+			Slider setWid = new Slider(10, 250, 10);
+			
+			ColorPicker cp = new ColorPicker();
+			
+			setHig.setValue(100);
+			setWid.setValue(100);
+			cp.setValue(Color.BLACK);
+			
+			Rectangle rec = new Rectangle(0,0,setWid.getValue(),setHig.getValue());
+			
+			ChangeListener<Number> ch = new ChangeListener<Number>(){
 
-			Polygon p1 = new Polygon(new double[] {
-					100,100,
-					120,80,
-					140,100,
-					160,80,
-					180,100,
-					200,80,
-					220,100,
-					220,50,
-					250,50,
-					250,100
-			});
-			Polygon p2 = new Polygon(new double[] {
-					100,100,
-					250,100,
-					250,140,
-					100,140
-			});
-			p1.setFill(Color.RED);
-			p1.setStroke(Color.BLACK);
-			p1.setStrokeWidth(2);
-			p2.setStroke(Color.BLACK);
-			p2.setStrokeWidth(3);
+				@Override
+				public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+					Rectangle rec = new Rectangle(0,0,setWid.getValue(),setHig.getValue());
+					rec.setFill(cp.getValue());
+					root.setCenter(rec);
+				}
+				
+			};
+			setHig.valueProperty().addListener(ch);
+			setWid.valueProperty().addListener(ch);
 			
+			cp.setOnAction(new EventHandler<ActionEvent>() {
+
+				@Override
+				public void handle(ActionEvent arg0) {
+					Rectangle rec = new Rectangle(0,0,setWid.getValue(),setHig.getValue());
+					rec.setFill(cp.getValue());
+					root.setCenter(rec);
+				}
+				
+			});
 			
-			root.getChildren().addAll(p1,p2);
+			set.getChildren().addAll(l1,setHig,l2,setWid,l3,cp);
+			
+			root.setTop(new Label("Rechteck-Konfigurator:"));
+			root.setCenter(rec);
+			root.setLeft(set);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("-");
